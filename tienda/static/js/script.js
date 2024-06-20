f32264c84e80dacb0f92a605228708cdc77487e4
Lenuam482 <man.rivas@duocuc.cl>
2024-06-19 22:07:19 -0400

    Actualizando proyecto a django parte 2

---
+++ tienda/static/js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    $("#registroForm").submit(function(event){
+
+        event.preventDefault();
+        
+
+        var usuario = $("#usuario").val();
+        var contraseña = $("#contraseña").val();
+        var verificarcontra = $("#verificarcontra").val();
+
+        if(usuario.length < 5 || usuario.length > 15){
+            alert("El nombre de usuario debe tener entre 5 y 15 caracteres.");
+            return;
+        }
+
+        if(contraseña.length < 8 || contraseña.length > 15){
+            alert("La contraseña debe tener entre 8 y 15 caracteres.");
+            return;
+        }
+
+        if(verificarcontra !== contraseña){
+            alert("Las contraseñas deben coincidir.");
+            return;
+        }
+
+
+        alert("¡Registro exitoso!");
+        document.getElementById("registroForm").reset();
+    });
+    
+});
+
+var cantidad = 0;
+var totalPrecio = 0;
+
+function agregarAlCarrito(nombre, precio) {
+
+    cantidad = cantidad + 1;
+
+    document.getElementById("cantidadCarro").innerText = "(" + cantidad + ")";
+
+    var nuevoProducto = document.createElement('div');
+    nuevoProducto.innerHTML = '<p>' + nombre + ' - $' + precio + " CLP" + '</p>';
+
+    var contenedorProductos = document.getElementById('listaProductos');
+    contenedorProductos.appendChild(nuevoProducto);
+
+    totalPrecio = totalPrecio + precio;
+
+    actualizarTotalPrecio();
+
+    mostrarOcultarMensajeCarrito();
+
+    var offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasRight'));
+    offcanvas.show();
+}
+
+function actualizarTotalPrecio() {
+
+    document.getElementById("totalPrecio").innerText = "$" + totalPrecio.toFixed(0) + " CLP";
+}
+
+function mostrarOcultarMensajeCarrito() {
+    var mensajeCarrito = document.getElementById('mensajeCarrito');
+    if (cantidad > 0) {
+        mensajeCarrito.style.display = "none";
+    } else {
+        mensajeCarrito.style.display = "block";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    mostrarOcultarMensajeCarrito();
+});
