5e6c736354d12749ca203d9a3dfb62b56926614d
Lenuam482 <man.rivas@duocuc.cl>
2024-06-19 22:13:40 -0400

    agregando vistas a django

--- tienda/urls.py
+++ tienda/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index,name='index')
+    path('index', views.index,name='index'),
+    path('darwin', views.darwin,name='darwin'),
+    path('tortuga', views.tortuga,name='tortuga'),
+    path('mapa', views.mapa,name='mapa'),
+    path('mascotas', views.mascotas,name='mascotas'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request,'tienda/index.html',context)
+
+def darwin(request):
+    context={}
+    return render(request,'tienda/darwin.html',context)
+
+def mascotas(request):
+    context={}
+    return render(request,'tienda/mascotas.html',context)
+
+def tortuga(request):
+    context={}
+    return render(request,'tienda/tortuga.html',context)
+
+def mapa(request):
+    context={}
+    return render(request,'tienda/mapa.html',context)
