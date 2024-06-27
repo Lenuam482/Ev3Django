31ea60414fcdc1bbe4d350fc625b97f8dc9d6282
Lenuam482 <man.rivas@duocuc.cl>
2024-06-27 17:30:15 -0400

    instalación de pillows

--- tienda/models.py
+++ tienda/models.py
@@ -7,3 +7,17 @@ class Usuario(models.Model):
     email = models.EmailField(unique=True, max_length=100, blank=True, null=True)
     def __str__(self):
         return str(self.username)
+    
+class carrito(models.Model):
+    codigo = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=20)
+    precio = models.IntegerField
+    cantidad = models.IntegerField
+    
+
+class producto(models.Model):
+    codigo = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField
+    imagen = models.ImageField(upload_to="producto", null=True)
+    descripcion = models.TextField()
