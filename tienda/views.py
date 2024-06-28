f8c28ed7d22fc8d5bb8af83e59b61ffefffc8f6b
Lenuam482 <man.rivas@duocuc.cl>
2024-06-27 23:08:03 -0400

    Agregando productos desde la base de datos

--- tienda/views.py
+++ tienda/views.py
@@ -1,29 +1,47 @@
 from django.shortcuts import render
-from .models import Usuario, Producto
+from .models import Usuario, Producto, Carrito, CarritoProducto
 # Create your views here.
 def index(request):
     usuario = Usuario.objects.all()
     productos = Producto.objects.all()
+    carrito = Carrito.objects.all()
     context={"usuario": usuario,
-             "productos": productos}   
+             "productos": productos,
+             "carrito" : carrito}   
     return render(request,'tienda/index.html',context)
 
 def darwin(request):
     usuario = Usuario.objects.all()
-    context={"usuario": usuario}
+    productos = Producto.objects.all()
+    carrito = Carrito.objects.all()
+    context={"usuario": usuario,
+             "productos": productos,
+             "carrito" : carrito}
     return render(request,'tienda/darwin.html',context)
 
 def mascotas(request):
     usuario = Usuario.objects.all()
-    context={"usuario": usuario}
+    productos = Producto.objects.all()
+    carrito = Carrito.objects.all()
+    context={"usuario": usuario,
+             "productos": productos,
+             "carrito" : carrito}
     return render(request,'tienda/mascotas.html',context)
 
 def tortuga(request):
     usuario = Usuario.objects.all()
-    context={"usuario": usuario}
+    productos = Producto.objects.all()
+    carrito = Carrito.objects.all()
+    context={"usuario": usuario,
+             "productos": productos,
+             "carrito" : carrito}
     return render(request,'tienda/tortuga.html',context)
 
 def mapa(request):
     usuario = Usuario.objects.all()
-    context={"usuario": usuario}
+    productos = Producto.objects.all()
+    carrito = Carrito.objects.all()
+    context={"usuario": usuario,
+             "productos": productos,
+             "carrito" : carrito}
     return render(request,'tienda/mapa.html',context)
