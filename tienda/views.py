8a781aba53881f46dc6e7da280e60a944eae9b7d
Lenuam482 <man.rivas@duocuc.cl>
2024-07-15 01:34:53 -0400

    Creando crud para productos

--- tienda/views.py
+++ tienda/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto, Carro
-from django.http import HttpResponse
-from django.views.decorators.http import require_POST
+from .forms import ProductoForm
 # Create your views here.
-
-def ad_productos(request):
-    carro = Carro.objects.all()
-    productos = Producto.objects.all()
-    context={"productos": productos,
-             "carro" : carro}   
-    return render(request,'tienda/ad_productos.html',context)
 
 def compra(request):
     carro = Carro.objects.all()
@@ -57,6 +49,12 @@ def mapa(request):
              "carro" : carro}
     return render(request,'tienda/mapa.html',context)
 
+def listar_producto(request):
+    productos = Producto.objects.all()
+    context = {'productos': productos}
+    return render(request, 'tienda/listar_producto.html', context)
+
+
 def agregar_al_carro(request):
 
     nombre = request.POST['nombre']
@@ -78,3 +76,66 @@ def eliminar_del_carro(request, nombre):
         carro = get_object_or_404(Carro, nombre=nombre)
         carro.delete()
     return redirect(request.META.get('HTTP_REFERER', '/'))
+
+def eliminar_producto(request,nombre):
+    if request.method == 'POST':
+        producto = get_object_or_404(Producto, nombre=nombre)
+        producto.delete()
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+def ad_productos(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('ad_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'tienda/ad_productos.html', {'form': form})
+
+def edicion_producto(request, pk):
+    if pk != "":
+        producto=Producto.objects.get(nombre=pk)
+        context={"productos": producto}
+
+        if producto:
+            return render(request, 'tienda/edicion_producto.html', context)
+        else:
+            context={'mensaje':"Error, nombre producto no existe"}
+            return render(request, 'tienda/edicion_producto.html', context)
+        
+def productoupdate(request):
+    if request.method == "POST":
+        nombre=request.POST["nombre"]
+        precio=request.POST["precio"]
+        descripcion=request.POST["descripcion"]
+        
+
+        producto = Producto()
+        producto.nombre=nombre
+        producto.precio=precio
+        producto.descripcion=descripcion
+        
+        if 'imagen' in request.FILES:
+                producto.imagen = request.FILES['imagen']
+                
+        producto.save
+        context={'mensaje':"Ok, producto actualizado",'productos': producto}
+        return render(request, 'tienda/listar_producto.html', context)
+    else:
+        producto = Producto.objects.all()
+        context = {'productos': producto}
+        return render(request, 'tienda/listar_producto.html', context)
+
+# def edicion_producto(request, nombre):
+#     producto = get_object_or_404(Producto, nombre=nombre)
+    
+#     if request.method == 'POST':
+#         form = ProductoForm(request.POST, request.FILES, instance=producto)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('listar_producto')  
+#     else:
+#         form = ProductoForm(instance=producto)
+    
+#     return render(request, 'edicion_producto.html', {'form': form, 'producto': producto})
